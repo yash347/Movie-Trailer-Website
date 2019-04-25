d86a636068ccca43a0a587f0575c7477f335cc43
yash347 <38907907+yash347@users.noreply.github.com>
2019-04-25 11:30:23 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,8 +7,13 @@ entertainment_center file"""
 
 fast_and_furious = media.Movie("Fast & Furious 8", 
                                "2hrs 29mins",
-                               "Dom encounters a mysterious woman, Cipher, who gets him involved in the world of terrorism. The crew has to reunite to stop Cipher and save the man who brought them together as a family.",
-                               "https://upload.wikimedia.org/wikipedia/en/2/2d/The_Fate_of_The_Furious_Theatrical_Poster.jpg",
+                               "Dom encounters a mysterious woman, "
+                               "Cipher, who gets him involved in the "
+                               "world of terrorism. The crew has to "
+                               "reunite to stop Cipher and save the "
+                               "man who brought them together as a family.",
+                               "https://upload.wikimedia.org/wikipedia/en/2"
+                               "/2d/The_Fate_of_The_Furious_Theatrical_Poster.jpg",
                                "https://www.youtube.com/watch?v=uisBaTkQAEs",
                                "PG")
 
@@ -16,34 +21,46 @@ fast_and_furious = media.Movie("Fast & Furious 8",
 avatar = media.Movie("Avatar", 
                      "2hrs 42mins", 
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/sco/b/"
+                     "b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY",
                      "G")
 
 john_wick = media.Movie("John Wick", 
                         "1hr 41mins", 
-                        "John Wick, a retired hitman grieving the loss of his wife, is forced to return to his old ways",
-                        "https://upload.wikimedia.org/wikipedia/en/9/98/John_Wick_TeaserPoster.jpg",
+                        "John Wick, a retired hitman grieving the "
+                        "loss of his wife, is forced to "
+                        "return to his old ways",
+                        "https://upload.wikimedia.org/wikipedia/en/9/98/"
+                        "John_Wick_TeaserPoster.jpg",
                         "https://www.youtube.com/watch?v=RllJtOw0USI",
                         "PG")
 
 the_perfect_date = media.Movie("The Perfect Date", 
                                "1hr 30mins",
-                               "A high school student creates an app to offer his services as a fake date to make money for college. When he develops feelings for someone, his plan gets complicated",
-                               "https://upload.wikimedia.org/wikipedia/en/7/7a/The_Perfect_Date.jpg",
+                               "A high school student creates an "
+                               "app to offer his services as a fake date "
+                               "to make money for college. When he develops feelings "
+                               "for someone, his plan gets complicated.",
+                               "https://upload.wikimedia.org/wikipedia/en/7/7a/"
+                               "The_Perfect_Date.jpg",
                                "https://www.youtube.com/watch?v=Hld-7oBn3Rk",
                                "R")
 
 avengers = media.Movie("Avengers: Endgame", 
                        "3hrs",
-                       "Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle.",
-                       "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg",
+                       "Adrift in space with no food or water, "
+                       "Tony Stark sends a message to Pepper "
+                       "Potts as his oxygen supply starts to dwindle.",
+                       "https://upload.wikimedia.org/wikipedia/en/0/0d/"
+                       "Avengers_Endgame_poster.jpg",
                        "https://www.youtube.com/watch?v=TcMBFSGVi1c&t=2s",
                        "G")
 
 spectre = media.Movie("Spectre", 
                       "2hrs 40mins",
-                      "James Bond receives an obscure message from M about a sinister organisation, 'SPECTRE'.",
+                      "James Bond receives an obscure message from "
+                      "M about a sinister organisation, 'SPECTRE'.",
                       "http://www.impawards.com/2015/posters/spectre.jpg",
                       "https://www.youtube.com/watch?v=7GqClqvlObY",
                       "PG")
